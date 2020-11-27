01d2608c5487ee41efab951b7dd3666e8ce4ea1a
senabay <senabayraktaroglu@novit.ai>
2020-11-27 12:31:59 +0300

    Using polyline draw rect

--- visualize_vgg_yolo_annotations.py
+++ visualize_vgg_yolo_annotations.py
@@ -16,13 +16,27 @@ def draw_polyline_vgg(img,pts_dene):
     cv2.polylines(img,[pts_dene],True,(255, 0, 0))
     return img 
 
+
+
+img = cv2.imread(path)
+
 #trying to draw polyline 
 x = [116,94,176,343,383,385,369,406,398,364,310,297,304,244,158]
 y = [157,195,264,273,261,234,222,216,155,124,135,170,188,170,175]
 
 pts_dene = make_x_y_as_pts(x,y)
+img = draw_polyline_vgg(img,pts_dene)
+print(max(x))
+print(max(y))
+print(min(x))
+print(min(y))
+x_rect= [min(x),max(x),max(x),min(x)]
+y_rect= [max(y),max(y),min(y),min(y)]
 
-
-img = cv2.imread(path)
-
+pts_rect = make_x_y_as_pts(x_rect,y_rect)
+img = draw_polyline_vgg(img,pts_rect )
 cv2.imwrite('image_dene.jpg', img) 
+
+#try to draw rect
+
+#
