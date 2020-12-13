d9fbe185c8d95d40b18f21f67eb9c9d3aa2a93f5
senabayraktaroglu <senaaa.bayraktar@gmail.com>
2020-12-13 15:08:47 +0300

    simple vgg format label created from https://www.robots.ox.ac.uk/~vgg/software/via/via_demo.html

--- simple_example_vgg_to_yolo.py
+++ simple_example_vgg_to_yolo.py
@@ -1,5 +1,15 @@
 from skimage import io
-
-bear_url = "https://upload.wikimedia.org/wikipedia/commons/7/71/2010-kodiak-bear-1.jpg"
+import cv2
+import json
+bear_url = "https://www.wildlifeworldwide.com/images/categories/bearwatching_brown-bear-backlit_finland.jpg"
 bear_image = io.imread(bear_url)
 
+#cv2.imshow("bear_image",bear_image )
+#cv2.waitKey()
+
+vgg_format_label_bear = '{"x":99,"y":191,"width":405,"height":373}'
+
+
+vgg_format_label_bear  = json.loads(vgg_format_label_bear ) 
+
+print(vgg_format_label_bear["x"])
